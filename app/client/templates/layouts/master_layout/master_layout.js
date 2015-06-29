2104c0ed0d0f676a8793ae573c4776e337d8e157
SophieV <Sophie@adoptive.co>
2015-06-28 22:30:49 -0400

    boostrap begin

--- app/client/templates/layouts/master_layout/master_layout.js
+++ app/client/templates/layouts/master_layout/master_layout.js
@@ -1,4 +1,13 @@
 Template.MasterLayout.helpers({
+	isActive: function(routeName){
+		var classValue = '';
+		if (Router.current().route.getName().toLowerCase() === routeName.toLowerCase())
+		{
+			classValue = "active";
+		}
+		
+		return classValue;
+	}
 });
 
 Template.MasterLayout.events({
